19139339dc18a993ec18bcadf7a1dbbef83855c9
Otilia-Torgby <otorgby@st.ug.edu.gh>
2024-07-12 20:15:51 +0000

    Modified the App.js file

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import HomeScreen from './Screens/HomeScreen';
+import CartScreen from './Screens/CartScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import ProductDetailScreen from './Screens/ProductDetailScreen';
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+   <NavigationContainer>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+<Stack.Navigator>
+    <Stack.Screen name='Home' component={HomeScreen}/>
+      <Stack.Screen name='Cart' component={CartScreen}/>
+      <Stack.Screen name='Product Details' component={ProductDetailScreen}/>
+
+
+</Stack.Navigator>
+     
+
+      
+
+    
+   </NavigationContainer>
+  );
+};
